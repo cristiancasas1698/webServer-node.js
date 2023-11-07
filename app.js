e6611fc2f4fad938e8f13c2eca014a36bc5c9e27
= <cristiancasas1698@gmail.com>
2023-11-06 20:45:42 -0500

    agregando conteido estatico html para mostrarlo en el lado del cliente

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 8080;
+// servir contenido estatico
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
 
 app.get('/hola-mundo', function (req, res) {
     res.send('hola mundo en su respectiva ruta')
-  });
+});
 
-  app.get('*', function (req, res) {
-    res.send('404 | page no found')
-  });
+
+
+app.get('/home', function (req, res) {
+    res.sendFile(__dirname+ '/public/home.html')
+});
+
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname+ '/public/generic.html')
+});
+
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname+ '/public/elements.html')
+});
+
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname +'/public/back/404.html');
+});
 
 
 
